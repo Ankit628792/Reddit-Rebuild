2ced7df87d59364962434cc3e21d690b08a20d53
Ankit <ankit628792@gmail.com>
2022-06-29 19:34:11 +0530

    progress bar and fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,18 @@ import Header from '../components/Header'
 import { ApolloProvider } from '@apollo/client'
 import client from '../applloClient'
 import { Toaster } from 'react-hot-toast'
+import Router from 'next/router'
+import ProgressBar from '@badrap/bar-of-progress'
+
+const progress = new ProgressBar({
+  size: 5,
+  className: "z-50 insta-gradient",
+  delay: 100
+})
+
+Router.events.on('routeChangeStart', progress.start)
+Router.events.on('routeChangeComplete', progress.finish)
+Router.events.on('routeChangeError', progress.finish)
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
@@ -13,6 +25,9 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
       <Head>
         <title>Reddit 2.0</title>
         <link rel="icon" href="/images/reddit_logo_sm.svg" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700;800&display=swap" rel="stylesheet" />
       </Head>
       <ApolloProvider client={client}>
         <SessionProvider session={session}>
