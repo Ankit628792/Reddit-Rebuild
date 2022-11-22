42167d336b255a65ef927258c766f211aff7cb53
Ankit Kumar <ankit@sugoilabs.com>
2022-11-22 20:04:00 +0530

    pwa

--- next.config.js
+++ next.config.js
@@ -1,7 +1,21 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  skipWaiting: true,
+  register: true,
+  disable: process.env.NODE_ENV === 'development'
+})
+
+
+module.exports = withPWA({
+  // next.js config
   reactStrictMode: true,
+  i18n: {
+    locales: ['en-US', 'fr', 'nl-NL'],
+    defaultLocale: 'en-US',
+  },
   images: {
     domains: ['links.papareact.com', 'avatars.dicebear.com']
   }
-}
+})
